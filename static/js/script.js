4913d72e846394ae18fd43099595fefae2d4788a
Alankar Shukla <alankarshukla9@gmail.com>
2021-10-05 12:41:28 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -19,7 +19,7 @@ $("#button").click(function(e) {
     var form_data = new FormData($('#upload-file')[0]);
     $.ajax({
         type: 'POST',
-        url: 'https://ede0-1-22-107-169.ngrok.io//upload_file/',
+        url: 'http://c35a-1-23-236-149.ngrok.io//upload_file/',
         beforeSend: function(){$('.loader').show();},
         complete: function(){ $('.loader').hide();},
         data: form_data,
